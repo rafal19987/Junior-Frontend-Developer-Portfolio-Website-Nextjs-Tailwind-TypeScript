73e150795a9be0381da74de6391588b8d9cd54f6
rafal19987 <rafal987.1@o2.pl>
2023-05-25 13:52:31 +0200

    update

--- app/components/About/SkillList.tsx
+++ app/components/About/SkillList.tsx
@@ -3,19 +3,17 @@ import Skill from './Skill';
 const SkillList = () => {
   return (
     <ol className="list-disc">
-      <Skill startX="30" delayTime={0.8} description="HTML, CSS/SASS" />
-      <Skill startX="40" delayTime={0.9} description="React, Next.js" />
+      <Skill startX="30" delayTime={0.8} description="HTML/BEM" />
       <Skill
-        startX="50"
-        delayTime={1}
-        description="Tailwind, Styled-components"
+        startX="40"
+        delayTime={0.9}
+        description="CSS/SASS/Styled-components/CSS Modules"
       />
-      <Skill
-        startX="60"
-        delayTime={1.1}
-        description="Conventional Commits, BEM"
-      />
-      <Skill startX="70" delayTime={1.2} description="Figma, Github" />
+      <Skill startX="50" delayTime={1} description="Tailwind" />
+      <Skill startX="60" delayTime={1.1} description="JavaScript/TypeScript" />
+      <Skill startX="70" delayTime={1.2} description="Next.js" />
+      <Skill startX="80" delayTime={1.3} description="Git/Github" />
+      <Skill startX="90" delayTime={1.4} description="Figma" />
     </ol>
   );
 };
