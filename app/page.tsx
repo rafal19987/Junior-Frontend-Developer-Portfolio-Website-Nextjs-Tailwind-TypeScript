0cada7c2fe6206b59faa3fdb4ae9781ce856afe7
Rafixx <rafal987.1@o2.pl>
2023-10-04 21:01:03 +0200

    feat: add new projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Experience from './components/Experience/Experience';
-import Projects from './components/Projects/Projects';
+import { Projects } from './components/Projects';
 import Contact from './components/Contact/Contact';
 
 const Page = () => {
   return (
-    <main className=" flex flex-col justify-center w-full h-full">
-      <div className="snap-y snap-mandatory h-[calc(100vh-3.5rem)] w-screen overflow-y-scroll overflow-x-hidden mt-14">
+    <main className=" flex flex-col justify-center w-full h-full ">
+      <div className="snap-y snap-mandatory h-[calc(100vh-3.5rem)] w-screen overflow-y-scroll overflow-x-hidden mt-14 ">
         <Home />
         <About />
         <Experience />
