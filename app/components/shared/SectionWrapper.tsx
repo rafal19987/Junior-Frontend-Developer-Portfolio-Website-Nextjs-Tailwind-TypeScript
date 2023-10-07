ce0968ebd337e31487dcf3b74b4d24d54af448f7
Rafixx <rafal987.1@o2.pl>
2023-10-07 22:58:35 +0200

    feat: apply intersection observer to sectionWrapper component

--- app/components/shared/SectionWrapper.tsx
+++ app/components/shared/SectionWrapper.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
+import { useAppContext } from '@/context/AppContext';
 import { cn } from '@/helpers/utils';
 
 export const SectionWrapper = ({
@@ -11,9 +15,44 @@ export const SectionWrapper = ({
   className?: string;
   children: React.ReactNode;
 }) => {
+  const { handleSelect } = useAppContext();
+  const sectionRef = useRef(null);
+
+  useEffect(() => {
+    const observerOptions = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.6,
+    };
+
+    const handleIntersection = (entries: IntersectionObserverEntry[]) => {
+      entries.forEach((entry: IntersectionObserverEntry) => {
+        if (entry.isIntersecting) {
+          handleSelect(entry.target.id);
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(
+      handleIntersection,
+      observerOptions,
+    );
+
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current);
+    }
+
+    return () => {
+      if (sectionRef.current) {
+        observer.unobserve(sectionRef.current);
+      }
+    };
+  }, []);
+
   return (
     <section
       id={id}
+      ref={sectionRef}
       className={`${
         id === 'home' && 'max-h-[calc(100vh-5rem)]'
       } flex flex-col items-center min-h-[calc(100vh-5rem)] px-6 scroll-mt-20`}
@@ -21,7 +60,9 @@ export const SectionWrapper = ({
       {title && (
         <div className='flex gap-8 items-center justify-center w-full h-full p-4'>
           <div className='w-1/12 h-1 bg-[#64FFDA]' />
-          <h2 className='text-2xl xl:text-3xl'>{title?.toUpperCase()}</h2>
+          <h2 className='text-2xl xl:text-3xl font-langar'>
+            {title?.toUpperCase()}
+          </h2>
           <div className='w-1/12 h-1 bg-[#64FFDA]' />
         </div>
       )}
