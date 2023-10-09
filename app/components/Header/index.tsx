0616e39b5960cc622c9e42dc96361815124b5f05
Rafixx <rafal987.1@o2.pl>
2023-10-09 12:34:03 +0200

    feat: add border on navbar

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -6,7 +6,7 @@ import logo from 'assets/headerLogo.svg';
 
 export const Header = () => {
   return (
-    <header className='fixed top-0 flex items-center justify-center w-full h-20 bg-[var(--body-bg-color)]  z-10'>
+    <header className='fixed top-0 flex items-center justify-center w-full h-20 bg-[var(--body-bg-color)] border-b border-gray-200 border-opacity-5  z-10'>
       <div className='relative flex items-center justify-between w-full max-w-[1400px] px-6 h-full'>
         <Image src={logo} width={50} height={50} alt='logo' />
         <Navbar />
