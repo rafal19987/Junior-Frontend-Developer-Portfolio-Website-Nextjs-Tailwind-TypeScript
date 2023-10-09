4f210eda7e166e18a46b9685405d48834d636162
Rafixx <rafal987.1@o2.pl>
2023-10-09 21:46:50 +0200

    fix: space on projects

--- app/components/Projects/index.tsx
+++ app/components/Projects/index.tsx
@@ -5,8 +5,8 @@ import { projects } from './content';
 
 export const Projects = () => {
   return (
-    <SectionWrapper id="projects" title="projects">
-      <div className="flex grow overflow-x-auto py-6 md:py-8  md:space-x-8 h-full w-full styledScroll">
+    <SectionWrapper id='projects' title='projects'>
+      <div className='flex grow overflow-x-auto py-6 md:py-8 space-x-4 md:space-x-8 h-full w-full styledScroll'>
         {projects.map((project) => (
           <Project key={project.id} project={project} />
         ))}
